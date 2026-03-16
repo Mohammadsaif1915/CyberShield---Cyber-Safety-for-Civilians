95802ebd618e8048a6f6b31a0e41d581f8f1fb0d
MOHAMMAD SAIF RAKHANGI <203634150+Mohammadsaif1915@users.noreply.github.com>
2026-03-16 11:02:37 +0530

    Resolved merge conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import CyberSafetyLanding from "./pages/Landing/CyberSafetyLanding.jsx";
-// import Register from "./components/Register.jsx";
-// import Login from './components/Login.jsx';
-// import ForgotPasswordPopup from './components/ForgotPasswordPopup.jsx';
-// import ResetPassword from './components/ResetPassword';
-// import About_us from "./components/About_us";
-
-// import Contact_cyber from "./components/Contact-cyber";
-// import Features_cyber from "./components/Features-cyber";
-// import Help_center from "./components/Help-center";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-
-//         <Route path="/" element={<CyberSafetyLanding />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/forgot-password" element={<ForgotPasswordPopup />} />
-//         <Route path="/reset-password/:token" element={<ResetPassword />} />
-//         <Route path="/about_us" element={<About_us />} />
-
-//         <Route path="/contact" element={<Contact_cyber />} />
-//         <Route path="/features" element={<Features_cyber />} />
-//         <Route path="/help-center" element={<Help_center />} />
-
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import CyberSafetyLanding from "./pages/Landing/CyberSafetyLanding";
@@ -41,12 +6,7 @@ import Login from "./components/Login";
 import ForgotPasswordPopup from "./components/ForgotPasswordPopup";
 import ResetPassword from "./components/ResetPassword";
 import About_us from "./components/About_us";
-<<<<<<< HEAD
 
-import Contact_cyber from "./components/Contact-cyber";
-import Features_cyber from "./components/Features-cyber";
-import Help_center from "./components/Help-center";
-=======
 import Features from "./components/Features";
 import Courses from "./components/Courses";
 import Community from "./components/Community";
@@ -54,8 +14,7 @@ import Our_Team from "./components/Our_Team";
 import Blog from "./components/Blog";
 import Help_Center from "./components/Help_Center"; 
 import Contact_Us from "./components/Contact_us";
-import Faq from "./components/Faq.jsx";
->>>>>>> e66201894e5818cdaba39841f3e195c83f6f9c8f
+import Faq from "./components/Faq";
 
 function App() {
   return (
@@ -69,15 +28,7 @@ function App() {
 
         <Route path="/forgot-password" element={<ForgotPasswordPopup />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-<<<<<<< HEAD
 
-        <Route path="/about-us" element={<About_us />} />
-
-        <Route path="/features" element={<Features_cyber />} />
-        <Route path="/contact" element={<Contact_cyber />} />
-        <Route path="/help-center" element={<Help_center />} />
-
-=======
         <Route path="/about_us" element={<About_us />} />
         <Route path="/features" element={<Features />} />
         <Route path="/courses" element={<Courses />} />
@@ -87,7 +38,7 @@ function App() {
         <Route path="/help_center" element={<Help_Center />} />
         <Route path="/contact_us" element={<Contact_Us />} />
         <Route path="/faq" element={<Faq />} />
->>>>>>> e66201894e5818cdaba39841f3e195c83f6f9c8f
+
       </Routes>
     </Router>
   );
